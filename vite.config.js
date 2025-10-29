3470d545c5113e3c932d6052157398d5945aa907
NIKOLAY VIKTOROVICH <145424511+NikolayViktorovich@users.noreply.github.com>
2025-10-29 17:16:34 +0500

    again..
    again..

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000
   }
 })
